c37f42630bc7ec42dba842aeeff69e3309539cc5
Eric Willhoit <e.willhoit@gmail.com>
2013-05-13 08:53:17 -0500

    adds include to personal manifest

--- modules/people/manifests/willhoit.pp
+++ modules/people/manifests/willhoit.pp
@@ -1,4 +1,4 @@
 class people::willhoit {
   notify { 'class people::willhoit declared': }
-  osx::recovery_message { 'If this Mac is found, please call 641-777-8185': }
+  include osx::recovery_message { 'If this Mac is found, please call 641-777-8185': }
 }
