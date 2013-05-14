e4bbd5b2e714e0003816657103a243cc11602609
Eric Willhoit <e.willhoit@gmail.com>
2013-05-14 08:00:22 -0500

    show all on desktop

--- modules/people/manifests/willhoit.pp
+++ modules/people/manifests/willhoit.pp
@@ -5,4 +5,7 @@ class people::willhoit {
 
 	# disable press-and-hold for accented character entry
 	include osx::global::disable_key_press_and_hold
+
+	# shows external hard drives, hard drives, mounted server and removable media on desktop
+	include osx::finder::show_all_on_desktop
 }
